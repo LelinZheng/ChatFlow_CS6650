ceb3ce7279759f69951d9688a7a2e7bbd9312ba3
LelinZheng <lezi.lelin@gmail.com>
2026-01-27 10:21:47 -0800

    Change directory name to match assignment

---
+++ server/src/test/java/edu/northeastern/cs6650/chat_server/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs6650.chat_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
