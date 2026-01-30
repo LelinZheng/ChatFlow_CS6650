c51873c5d8595f6f940ebc465d67e3489328b3aa
LelinZheng <lezi.lelin@gmail.com>
2026-01-29 17:07:46 -0800

    Initialize Client and add ChatMessage class

---
+++ client-part1/src/main/java/edu/northeastern/cs6650/client/model/ChatMessage.java
@@ -0,0 +1,61 @@
+package edu.northeastern.cs6650.client.model;
+
+public class ChatMessage {
+  private String userId;
+  private String username;
+  private String message;
+  private MessageType messageType;
+  private String timestamp;
+
+  public ChatMessage() {
+  }
+
+  public ChatMessage(String userId, String username, String message, MessageType messageType,
+      String timestamp) {
+    this.userId = userId;
+    this.username = username;
+    this.message = message;
+    this.messageType = messageType;
+    this.timestamp = timestamp;
+  }
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public MessageType getMessageType() {
+    return messageType;
+  }
+
+  public void setMessageType(MessageType messageType) {
+    this.messageType = messageType;
+  }
+
+  public String getTimestamp() {
+    return timestamp;
+  }
+
+  public void setTimestamp(String timestamp) {
+    this.timestamp = timestamp;
+  }
+}
