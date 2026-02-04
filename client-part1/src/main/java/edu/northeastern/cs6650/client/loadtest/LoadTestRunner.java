7a614532b3d21b00b1f6c7c8807f8a5624cddcdd
LelinZheng <lezi.lelin@gmail.com>
2026-02-03 16:22:50 -0800

    refactor LoadTestClient to have warmup and mainphase separated in a different class of LoadTestRunner

---
+++ client-part1/src/main/java/edu/northeastern/cs6650/client/loadtest/LoadTestRunner.java
@@ -0,0 +1,90 @@
+package edu.northeastern.cs6650.client.loadtest;
+
+import edu.northeastern.cs6650.client.model.ChatMessage;
+import edu.northeastern.cs6650.client.worker.MessageGenerator;
+import edu.northeastern.cs6650.client.worker.MessageSender;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LoadTestRunner {
+
+  private final URI baseUri;
+
+  public LoadTestRunner(URI baseUri) {
+    this.baseUri = baseUri;
+  }
+
+  public void runWarmup() {
+    int warmupThread = 32;
+    int warmupMessagesPerThread = 1000;
+    int warmupTotalMessages = warmupThread * warmupMessagesPerThread;
+    int queueCapacity = 50_000;
+
+    AtomicInteger idx = new AtomicInteger(0);
+    ExecutorService pool = Executors.newFixedThreadPool(warmupThread, r -> {
+      Thread t = new Thread(r);
+      t.setName("sender-" + idx.getAndIncrement());
+      return t;
+    });
+
+    BlockingQueue<ChatMessage> warmupQueue = new ArrayBlockingQueue<>(queueCapacity);
+    Thread generator = new Thread(new MessageGenerator(warmupQueue, warmupTotalMessages));
+
+    List<MessageSender> senderTasks = new ArrayList<>();
+    List<Future<?>> futures = new ArrayList<>();
+
+    long start = System.nanoTime();
+
+    generator.start();
+
+    for (int i = 0; i < warmupThread; i++) {
+
+      // Have all messages send to room1 for simplicity
+      URI fullUri = baseUri.resolve("1");
+      MessageSender sender = new MessageSender(warmupQueue, fullUri, warmupMessagesPerThread);
+      senderTasks.add(sender);
+
+      futures.add(pool.submit(sender));
+    }
+
+    try {
+      generator.join();
+      for (Future<?> f : futures) {
+        f.get();
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    } catch (ExecutionException e) {
+      e.getCause().printStackTrace();
+    }
+
+    pool.shutdown();
+
+    long end = System.nanoTime();
+
+    int ok = senderTasks.stream().mapToInt(MessageSender::getSentOk).sum();
+    int failed = senderTasks.stream().mapToInt(MessageSender::getSentFailed).sum();
+    double durationSeconds = (end - start) / 1_000_000_000.0;
+    double throughput = ok / durationSeconds;
+
+    System.out.println("Warmup done.");
+    System.out.println("OK=" + ok + " failed=" + failed);
+    System.out.println("timeSec=" + durationSeconds);
+    System.out.println("throughput msg/s=" + throughput);
+  }
+
+  public void runMainPhase() {
+
+  }
+
+  public void printSummary() { }
+
+}
