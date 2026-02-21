0cec2df88774cb99256256d1491b0260115c61d1
LelinZheng <lezi.lelin@gmail.com>
2026-02-20 23:12:28 -0800

    Revert "add junit tests"
    This reverts commit 915f797188b2a629470b74dd33522958c7c2e44a.

--- server/src/test/java/edu/northeastern/cs6650/chat_server/ChatServerApplicationTests.java
+++ server/src/test/java/edu/northeastern/cs6650/chat_server/ChatServerApplicationTests.java
@@ -1,36 +1,13 @@
 package edu.northeastern.cs6650.chat_server;
 
-import static org.mockito.Mockito.mock;
-
-import edu.northeastern.cs6650.chat_server.config.ChannelPool;
-import edu.northeastern.cs6650.chat_server.config.RabbitMQConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 
 @SpringBootTest
 class ChatServerApplicationTests {
 
-	@TestConfiguration
-	static class MockRabbitConfig {
-
-		@Bean
-		@Primary  // overrides the real RabbitMQConfig bean
-		public RabbitMQConfig rabbitMQConfig() {
-			return mock(RabbitMQConfig.class);
-		}
-
-		@Bean
-		@Primary  // overrides the real ChannelPool bean
-		public ChannelPool channelPool() {
-			return mock(ChannelPool.class);
-		}
-	}
-
 	@Test
 	void contextLoads() {
-		// Passes if the Spring context loads without errors
 	}
+
 }
