6b53041df81a2b3a0d3d39f3a7db6a753b1cdea6
LelinZheng <lezi.lelin@gmail.com>
2026-02-23 16:36:12 -0800

    set up consumer package

---
+++ consumer/src/test/java/edu/northeastern/cs6650/consumer/TestConsumerApplication.java
@@ -0,0 +1,11 @@
+package edu.northeastern.cs6650.consumer;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(ConsumerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
