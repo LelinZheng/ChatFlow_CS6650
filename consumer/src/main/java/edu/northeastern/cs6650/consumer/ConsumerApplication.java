6033e495a34940a0e0630a18e6d6496bdf7ff51f
LelinZheng <lezi.lelin@gmail.com>
2026-02-23 21:20:02 -0800

    add health rest endpoint

--- consumer/src/main/java/edu/northeastern/cs6650/consumer/ConsumerApplication.java
+++ consumer/src/main/java/edu/northeastern/cs6650/consumer/ConsumerApplication.java
@@ -1,7 +1,10 @@
 package edu.northeastern.cs6650.consumer;
 
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class ConsumerApplication {
@@ -9,5 +12,4 @@ public class ConsumerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
-
 }
